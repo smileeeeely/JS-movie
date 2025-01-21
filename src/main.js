5bb571c87f1365d664f9e65838919c5ba289fd84
smileeeeely <minjoole.ivy@gmail.com>
2025-01-21 10:50:17 +0900

    피드백 받은 후 수정

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+const BASE_URL = "https://api.themoviedb.org/3";
 const searchBtn = document.querySelector("#searchBtn");
 const searchInput = document.querySelector("#search");
 const resultUl = document.querySelector("#cards");
@@ -17,65 +18,67 @@ const options = {
 
 /** 영화 포스터 이미지 불러올 base URL 가져오기
 https://developer.themoviedb.org/reference/configuration-details */
-async function getPosterImageBaseUrl() {
-  const response = await fetch(
-    "https://api.themoviedb.org/3/configuration",
-    options
-  );
+async function getPosterImageBaseUrl() { const response = await fetch(`${BASE_URL}/configuration`, options);
   return await response.json();
 }
 
 /**  TMDB API로 인기 영화 데이터 가져오기
 https://developer.themoviedb.org/reference/movie-popular-list*/
 async function getPopularMoviesData(page = 1) {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?language=ko-KOR&page=${page}`,
-    options
-  );
-
+  const response = await fetch(`${BASE_URL}/movie/popular?language=ko-KOR&page=${page}`, options);
   return await response.json();
 }
-// search 데이터로 다른 영화 리스트도 다 가져오기
-// query,,,param,,
 
 /** 받아온 데이터 카드 리스트로 바꾸기 */
-async function makeDataToCardList() {
+async function makeDataToCardDataList() {
   const movies = await getPopularMoviesData();
   const baseImageUrl = await getImageBaseUrl();
-  const cardList = [];
+  const cardDataList = [];
 
   for (let i = 0; i < movies.results.length; i += 1) {
-    const card = movies.results[i];
-    card.poster_url = `${baseImageUrl}/${card.poster_path}`;
+    const cardData = movies.results[i];
+    cardData.poster_url = `${baseImageUrl}/${cardData.poster_path}`;
 
-    cardList.push(card);
+    cardDataList.push(cardData);
   }
 
-  return cardList;
+  return cardDataList;
 }
 
 /** 카드리스트 화면에 띄우기 */
 async function renderCardList(list) {
+  const imgUrl = await getImageBaseUrl();
   let movieCards = "";
 
   for (let i = 0; i < list.length; i++) {
-    movieCards += `<div id="${list[i].id}" class="card" >
-    <img id="${list[i].id}" class="cardImg" src= "${list[i].poster_url}">
+    movieCards += `
+    <div id="${list[i].id}" class="card" >
+    <img id="${list[i].id}" class="cardImg" src= "${imgUrl}/${list[i].poster_path}"></img>
     <p id="${list[i].id}" class="cardTitle">제목 : ${list[i].title}</p>
     <p id="${list[i].id}" calss="cardVoteAverage">평점 : ${list[i].vote_average}</p> </div>`;
   }
   resultUl.innerHTML = movieCards;
 }
-//index값 보다는 data mdn로 id 값 주기
 
 /** image의 base url 받아오기 */
 async function getImageBaseUrl(size = 1) {
   const configuration = await getPosterImageBaseUrl();
-
-  const baseUrl =
-    configuration.images.base_url + configuration.images.poster_sizes[size];
+  const baseUrl = configuration.images.base_url + configuration.images.poster_sizes[size];
 
   return baseUrl;
+}
+
+/** 검색 api 받아오기 
+https://developer.themoviedb.org/reference/search-movie */
+async function getSearchMoviesData(input) {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?query=${input}&include_adult=false&language=ko-KOR`,
+    options
+  );
+
+  const res = await response.json();
+  // console.log('new funtion: ', res.results);
+  renderCardList(res.results);
 }
 
 /** 검색 value 받아오기 */
@@ -83,78 +86,53 @@ searchBtn.addEventListener("click", function () {
   let input = searchInput.value;
   if (input.length !== 0) {
     input = input.toUpperCase();
-  } else {
-    alert("검색 결과가 없습니다.");
   }
-  getSearchInputList(input);
+  getSearchMoviesData(input);
 });
 // 엔터로 했을때도 검색이 되게
 
-/** input으로 들어온 검색어가 들어가는 영화들만 리스트에 나타나게 한다 */
-async function getSearchInputList(input) {
-  console.log(input);
-  const list = await makeDataToCardList();
-
-  const newList = list.filter(function (movie) {
-    const originalTitle = movie.original_title.toUpperCase();
-    if (originalTitle.includes(input)) {
-      return input;
-    }
-    return movie.title.includes(input);
-  });
-  if(newList.length === 0){
-    location.reload(true);
-    alert("검색어와 일치하는 영화가 없습니다.");
-  }
-  renderCardList(newList);
-}
-
-/** 영화 카드 클릭했을 때 상세 페이지 카드 팝업 */
+/** 영화 카드 클릭했을 때 모달 카드 팝업 */
 resultUl.addEventListener("click", function (e) {
   if (e.target.id !== "cards") {
-      modal.style.display = "block";
-      getTargetCardData(e.target.id);
-  }
+    modal.style.display = "block";
+    getTargetCardData(e.target.id);}
 });
 
-/** 클릭한 영화 카드의 데이터 가져오기 */
-async function getTargetCardData(targetId) {
-  // console.log(targetId);
-  const list = await makeDataToCardList();
-  // console.log(list);
-  const test = list.filter((el) => el.id == targetId);
-  renderPopUpCardDetail(test[0]);
+/** 클릭한 영화 카드의 데이터 가져오기 
+https://developer.themoviedb.org/reference/movie-details */
+async function getTargetCardData(target) {
+  const response = await fetch(`${BASE_URL}/movie/${target}?language=ko-KOR`, options);
+  const result = await response.json();
+  await renderPopUpCardDetail(result);
 }
 
 /** 모달 상세페이지에 나올 내용 붙이기 */
-function renderPopUpCardDetail(data) {
+async function renderPopUpCardDetail(data) {
+  const imgUrl = await getImageBaseUrl();
   console.log(data);
   const modal_content = document.querySelector(".modal-content");
-  let card = "";
-  card = `
+  modal_content.innerHTML = `
+    <img class="cardImg" src= "${imgUrl}/${data.poster_path}"></img>
     <span class="closeBtn">&times;</span>
-    <img class="cardImg" src= "${data.poster_url}">
     <h2 id="modal-title">${data.title}</h2>
     <p>${data.release_date} </p>
     <p id="modal-body">개봉일 : ${data.overview}</p>
     <p>평점 : ${data.vote_average}</p>
   `;
-  modal_content.innerHTML = card;
 }
 
 /** 모달 닫기 버튼 동작 */
 document.body.addEventListener("click", function (e) {
-  if(e.target.classList.contains("closeBtn"))
-  modal.style.display = "none";
+  if (e.target.classList.contains("closeBtn")) modal.style.display = "none";
 });
 
 /** 페이지 타이틀 클릭하면 메인 화면으로 돌아가기 */
-pageTitle.addEventListener("click", function(){
+pageTitle.addEventListener("click", function () {
   location.reload(true);
-})
+});
 
 async function main() {
-  const list = await makeDataToCardList();
+  const list = await makeDataToCardDataList();
 
   await renderCardList(list);
 }
